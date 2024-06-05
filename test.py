8366bfe133520f4bd51a6eb43e10846e966cf027
Tamim <farman.arefin1@outlook.com>
2024-06-05 10:35:46 +0600

    Added test.py

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-def factorial(n):
-    if(n==0):
-        return 1;
-    return n*factorial(n-1)
+s = range(12)
+even = []
+odd = []
+
+for x in s:
+    if(x%2==0):
+        even.append(x)
+    else:
+        odd.append(x)
+
+for i in range(len(even)):
+    print(even[i],end=' ')
+print('\n')
+for i in range(len(odd)):
+    print(odd[i],end=" ")
