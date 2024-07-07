7c84663f26f042a6d0e28134ea77de93950cb82f
Tamim <farman.arefin1@outlook.com>
2024-07-07 12:48:55 +0600

    New Code

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
-s = range(12)
-even = []
-odd = []
+def factorial(n):
+    if(n==0):
+        return 1
+    return n*factorial(n-1)
 
-for x in s:
-    if(x%2==0):
-        even.append(x)
-    else:
-        odd.append(x)
 
-for i in range(len(even)):
-    print(even[i],end=' ')
-print('\n')
-for i in range(len(odd)):
-    print(odd[i],end=" ")
+# class Person:
+#     def __init__(self,name, age):
+#         self.name = name
+#         self.age = age
+#
+# p1=Person("Tamim",25)
+# print(p1.name)
+# print(p1.age)
+
+def is_palindrome(input_string):
+    new_string=""
+    reverse_string=""
+
+
+    for letter in input_string:
+        if letter!="":
+            new_string=new_string+letter.lower()
+            reverse_string=letter.lower()+reverse_string
+
+    if new_string == reverse_string:
+        return True
+    return False
+
+
+
+def nametag(first_name,last_name):
+    return("{} {}.".format(first_name,last_name[0]))
+
+
+print(nametag("Farman","Arefin"))
